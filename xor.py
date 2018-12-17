143665c1255f438ace7a93efdda377275ffb5c4b
yoidea <yoidea.cc@gmail.com>
2018-12-17 18:24:45 +0900

    Add input functions

--- xor.py
+++ xor.py
@@ -8,6 +8,7 @@ class Model:
     def __init__(self):
         self.x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
         self.y = np.array([0, 1, 1, 0])
+        self.test = [0, 0]
         self.is_busy = False
         self.score = [0, 0]
 
@@ -26,12 +27,31 @@ class Model:
         self.score = self.model.evaluate(self.x, self.y)
 
 
+    def predict(self):
+        if hasattr(self, 'model') and not self.is_busy:
+            x = np.array([self.test])
+            y = self.model.predict(x)
+            return y[0][0]
+        return "None"
+
+
 @command("start training")
 def train():
     model.is_busy = True
     model.build()
     model.train()
     model.is_busy = False
+    print(model.predict())
+
+
+@command("set x1 %n")
+def set_x1(n):
+    model.test[0] = n
+
+
+@command("set x2 %n")
+def set_x2(n):
+    model.test[1] = n
 
 
 @predicate("is busy")
@@ -42,6 +62,11 @@ def is_busy():
 @reporter("get score")
 def get_score():
     return "%d" % (model.score[1] * 100)
+
+
+@reporter("predict")
+def predict():
+    return model.predict()
 
 
 if __name__ == "__main__":
